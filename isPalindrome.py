f97a46e4e7779ec258b2101d7e04db444ac09b1d
Sam Wareing <samantha.wareing@gmail.com>
2020-08-28 22:16:52 -0700

    My Final Solution

---
+++ isPalindrome.py
@@ -0,0 +1,48 @@
+##############################################################################################
+# File: isPalindrome.py    
+# Author: Sam Wareing
+# Description: determines if a string is a palindrome, considering only A-Z characters
+#
+# Input: String  
+#
+# Output: Boolean                               
+#
+#
+##############################################################################################
+import sys
+import re
+
+
+def isPalindrome(string): 
+    # use regular expression to remove all non-alpha characters
+    regex = re.compile('[^a-zA-Z]')     # a character not equal to a-z and A-Z
+    string = regex.sub('', string)      # substitute anything matching regex in string 
+    string = string.lower()             # make string all lowercase 
+
+    # invert string 
+    invStr = ""
+
+    for c in string: 
+        invStr = c + invStr
+
+    print(string) 
+    print(invStr) 
+
+    if invStr == string:
+        return True
+    else:
+        return False
+    
+
+
+def usage():
+    print("usage: " + sys.argv[0] + " string") 
+
+if __name__ == "__main__":
+    # check if an arguement was passed
+    if len(sys.argv) < 2: 
+        usage()
+        exit() 
+    
+    print(isPalindrome(sys.argv[1]))
+
