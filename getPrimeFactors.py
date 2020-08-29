c6f73bf62dbb241c252fe8a20c40194e74bdf5c5
Sam Wareing <samantha.wareing@gmail.com>
2020-08-28 21:05:40 -0700

    Added function to check for prime numbers

--- getPrimeFactors.py
+++ getPrimeFactors.py
@@ -11,22 +11,45 @@
 ##############################################################################################
 import sys
 
-def get_prime_factors(number):
-    prime_factorization = []
-    print(str(number)) 
+def getPrimeFactors(number):
+    primeFactorization = []
 
-    return prime_factorization
+    print("finding prime factorization for " + str(number)) 
+
+    if isPrime(number): 
+        print(str(number) + " is prime") 
+    else:
+        print(str(number) + " is not prime") 
+    
+
+    return primeFactorization
+
+def isPrime(n):
+    # if the passed number is greater than 1 and is a whole number 
+    # check if the number can be evenly divided by a number 2 to n-1 
+    # i.e. a number other than 1 and n 
+    if n > 1 and n % 1 == 0:
+        for i in range(2, n-1):
+            if n % i == 0:
+                return False
+
+    return True
+
 def usage():
-    print("usage: " + sys.argv[0] + " integer") 
+    print("usage: " + sys.argv[0] + " int") 
 
 if __name__ == "__main__":
+    # check if an arguement was passed
     if len(sys.argv) < 2: 
         usage()
         exit() 
+
+    # check if the arguement passed is an int
     try: 
         number = int(sys.argv[1])
     except: 
         usage()
         exit()
 
-    print(get_prime_factors(number)) 
+    # print the prime factorization or the passed in int
+    print(getPrimeFactors(number)) 
