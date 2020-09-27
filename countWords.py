35e37e3c46a68e82125aafb30799016889e36884
Samantha Wareing <samantha.wareing@gmail.com>
2020-09-26 19:59:30 -0700

    my final solution

---
+++ countWords.py
@@ -0,0 +1,28 @@
+##############################################################################################
+#
+#
+##############################################################################################
+import sys
+import re
+from collections import Counter
+
+def countWords(filePath): 
+	# find all words with letters, numbers, hyphens, and/or apostrophes 
+	words = re.findall(r"\w+|[']\w+|\w+[']|\w+['-]*\w+", open(filePath).read().lower())
+	top_words = Counter(words).most_common(20)
+	
+	print("\nWORD\tCOUNT")
+	for word in top_words:
+		print("{}\t{:d}".format(word[0], word[1]))
+
+def usage(): 
+    print("usage: countWords.py /path/to/text/file")
+
+if __name__ == "__main__":
+	if len(sys.argv) < 2: 
+		usage()
+		exit()
+
+	countWords(sys.argv[1])
+
+  
