a3ff386df1af7e767513b84b2a3e71be6ce0da14
Samantha Wareing <samantha.wareing@gmail.com>
2020-09-26 19:21:59 -0700

    updated solution to use Counter() and some other neat tricks from the learning module

--- diceSim.py
+++ diceSim.py
@@ -7,43 +7,17 @@
 #
 ##############################################################################################
 import sys
-import os
-import random
+from random import randint
+from collections import Counter
 
-num_simulations = 1000000
+def simulateDiceRolls(dice, num_simulations):
+	counts = Counter()
+	for roll in range(num_simulations):
+		counts[sum((randint(1, sides) for sides in dice))] += 1
 
-def simulateDiceRolls(dice):
-	# initialize dictionary to track roll values of dice
-	dice_rolls = {} 
-	start = len(dice)
-	stop = 0
-	for di in dice: stop+=di
-
-	for i in range(start, stop+1):
-		dice_rolls[i] = 0
-
-	# simulate dice rolls
-	for i in range(1, num_simulations+1):
-		roll = 0
-		for di in dice: 
-			roll += random.randint(1, di)
-		dice_rolls[roll]+=1
-
-	return dice_rolls
-
-def calculateProbabilty(dice_rolls):
-	probability = {} 
-
-	for roll in dice_rolls.keys(): 
-		probability[roll] = float(dice_rolls[roll])/num_simulations*100
-
-	return probability
-
-
-def printResults(probability):
-	for roll in probability.keys(): 
-		percent = str(probability[roll]) + "%"
-		print(str(roll) + ": " + percent)
+	print("\nOUTCOME\tPROBABILITY")
+	for outcome in range(len(dice), sum(dice)+1): 
+		print('{}\t{:0.2f}%'.format(outcome, counts[outcome]*100/num_simulations))
 
 
 def usage(): 
@@ -55,13 +29,18 @@ if __name__ == "__main__":
 	if len(sys.argv) < 2: 
 		usage()
 		exit()
-	num_dice = len(sys.argv) - 1
-	dice = []
-	for i in range(1, num_dice+1): dice.append(int(sys.argv[i]))
 
-	print(dice)
+	num_simulations = input("How many simulations? press enter for default 1000000 ")
+	
+	if num_simulations == "": 
+		num_simulations = 1000000
+	else:
+		num_simulations = int(num_simulations)
 
-	dice_rolls = simulateDiceRolls(dice)
-	probability = calculateProbabilty(dice_rolls) 
-	printResults(probability)
+	n = len(sys.argv)
+	dice = [int(sys.argv[i]) for i in range(1, n)]
+
+	simulateDiceRolls(dice, num_simulations)
+
+
   
